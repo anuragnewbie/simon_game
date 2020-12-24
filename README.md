1356736299c1b12ac40264b94ff6d54523b89089
neil95 <anuragchatterjee4@gmail.com>
2020-12-24 21:33:03 +0530

    uploading all files of simon game project

---
+++ README.md
@@ -0,0 +1,16 @@
+This is a simon game project.
+
+I have developed this game using html5, css, bootstrap4 and jquery.
+
+I have developed this game during taking web development class at Udemy.
+
+Thanx to Angela Yu ma'am.
+
+It's such a game where there will be a master called as Simon and whatever
+command he will give the other three members will need to follow. The three
+members need to remeber the sequence of the game pattern.
+
+Hope you will like it and if you want to modify it or debug my code pls
+feel free to do that.
+
+Thank you. 😀
---
+++ game.js
@@ -0,0 +1,82 @@
+var buttonColours = ["red", "blue", "green", "yellow"], gamePattern = [], userClickedPattern = [], level=0, started=true;
+
+// game starts by pressing a key once...
+function game_start () {
+	$("body").on("keypress", function () {
+		if(started === true)
+			nextSequence(), started=false;
+	});
+}
+
+game_start();
+
+// button clicks to play the game and promote to next levels...
+$(".btn").on("click", function () {
+	var userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+
+    playSound(this.id);
+	animatePress(this.id);
+
+	generateSequence(userClickedPattern.length-1);
+});
+
+// here the mapping of the user selected button color with the color selected by the system and if it matches then level increases else error...
+function generateSequence (currentLevel) {
+	if(gamePattern[currentLevel] === userClickedPattern[currentLevel])
+	{
+		if(userClickedPattern.length === gamePattern.length)
+		{
+			console.log("correct");
+			setTimeout(function () {
+				nextSequence();
+			}, 1000);
+		}
+	}
+	else
+	{
+		console.log("wrong");
+	 	$("body").addClass("game-over");
+	 	setTimeout(function () {
+	 		$("body").removeClass("game-over");
+	 	}, 200);
+	 	playSound("wrong");
+	 	$("h1#level-title").text("Game Over, Press Any Key to Restart");
+	 	startOver();
+	}
+}
+
+// here the game will start only after the user loses the game...
+function startOver () {
+	level=0;
+	gamePattern=[];
+	started=true; 
+}
+
+// here the game level will be increased...
+function nextSequence () {
+	userClickedPattern=[];
+	level += 1;
+	$("h1#level-title").text("Level " + level);
+
+	var randomNumber = Math.floor(Math.random() * 4);
+	var randomChosenColor = buttonColours[randomNumber];
+	gamePattern.push(randomChosenColor);
+
+	playSound(randomChosenColor);
+    animatePress(randomChosenColor);
+}
+
+// sounds will be played on button clicks and key press...
+function playSound (Colorname) {
+    var a = new Audio("sounds/" + Colorname + ".mp3");
+    a.play();
+}
+
+// background effect will take place on button clicks and key press...
+function animatePress (currentColor) {
+	$("#"+currentColor).addClass("pressed");
+	setTimeout(function() {
+		$("#" + currentColor).removeClass("pressed");
+	},100);
+}
